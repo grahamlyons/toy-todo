80359654056ed73de555539c257934d2eb630f6c
grahamlyons <graham@grahamlyons.com>
2019-09-12 21:22:40 +0100

    Make style changes and config for linter
    Error introduced after adding Cypress.

--- cypress/integration/todo_spec.js
+++ cypress/integration/todo_spec.js
@@ -1,16 +1,16 @@
-describe('Toy todo app', function() {
-  it('title contains "Toy"', function() {
-    cy.visit('/')
+describe('Toy todo app', () => {
+  it('title contains "Toy"', () => {
+    cy.visit('/');
 
     cy.get('h1').should('contain', 'Toy');
-  })
+  });
 
-  it('added message appears on the page', function() {
-    cy.visit('/')
+  it('added message appears on the page', () => {
+    cy.visit('/');
 
     cy.get('input[type=text]').type('A message');
     cy.get('input[type=submit]').click();
 
     cy.get('li').should('contain', 'A message');
-  })
-})
+  });
+});
