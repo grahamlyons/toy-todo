1da04301440637f9e840318a9ca8620df01d6b45
grahamlyons <graham@grahamlyons.com>
2019-09-12 22:44:00 +0100

    Configure production to use URL for DB connection

--- config/config.js
+++ config/config.js
@@ -8,10 +8,7 @@ module.exports = {
     storage: ':memory:',
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     operatorsAliases: false,
   },
